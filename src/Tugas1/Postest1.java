032afe465b103d9a3ae9a20d07371501fa75c239
FetrisyeDP <fetrisyedelp@gmail.com>
2017-03-08 16:28:51 +0800

    Tugas 2 Selesai

--- src/Tugas1/Postest1.java
+++ src/Tugas1/Postest1.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tugaspostest1;
+package Tugas1;
 
 /**
  *
  * @author Asus
  */
-public class TugasPostest1 {
+public class Postest1 {
 
     /**
      * @param args the command line arguments
